6c48270e31ee7a0809644840320c77a4c512c477
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-08-24 16:21:23 -0700

    Numbers Challenge

---
+++ NumbersChallenge/NumbersChallenge/NumbersChallenge.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float num1 = 1.5f;
+
+    float num2 = 3.75f;
+
+    float num3 = -2.25f;
+
+
+    cout << "Num1: " << num1 << '\n' << "Num2: " << num2 << '\n' << "Num3: " << num3 << '\n';
+
+    cout << "Here is the sum of num1 and num2 and the difference using num3 on the prior sum: " << num1 + num2 - num3;
+}
