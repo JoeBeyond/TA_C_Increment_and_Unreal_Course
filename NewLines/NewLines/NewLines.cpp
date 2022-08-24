1d6ea4e3f4312806548187d610fe67521d8fdbfa
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-08-23 17:43:20 -0700

    New Lines Challenge

---
+++ NewLines/NewLines/NewLines.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    // I'm trying different examples.
+
+    using namespace std;
+
+    cout << "10 Banannas\n+ 7 Apples" << '\n';
+
+    cout << 250 << endl << "Kiwis" << '\n';
+
+    cout << 5000 << " Lychees" << endl;
+
+    '\n';
+
+    cout << 2175 << " Oranges";
+
+    cout << endl;
+}
+
+
