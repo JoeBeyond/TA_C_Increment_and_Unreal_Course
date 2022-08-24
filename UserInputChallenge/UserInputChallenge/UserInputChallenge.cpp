ce29c108f5dc780d1f0940bb3408b88ee2665a85
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-08-24 16:11:30 -0700

    User Input Challenge

---
+++ UserInputChallenge/UserInputChallenge/UserInputChallenge.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int compInt = 7, userInt;
+
+	cout << "Please type a number: ";
+
+	cin >> userInt;
+
+	cout << "This is your new number!: " << compInt * userInt;
+}
