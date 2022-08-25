6c4da98486285c4da0a9266a928d0059c73e2015
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-08-24 22:42:48 -0700

    Comparison Operators Challenge

---
+++ ComparisonOperatorsChallenge/ComparisonOperatorsChallenge/ComparisonOperatorsChallenge.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 11;
+
+    int num2 = 23;
+
+    int num3 = 35;
+    
+    int num4 = 47;
+
+    if (num2 > num1)
+    {
+        cout << "Success1A \n";
+    }
+
+    if (num3 == num3)
+    {
+        cout << "Success2B \n";
+    }
+
+    if (num1 != num3)
+    {
+        cout << "Success3C \n";
+    }
+
+    if (num3 <= num4)
+    {
+        cout << "Success4D \n";
+    }
+}
