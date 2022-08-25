360ec0f3a1de66adf2e0282f2ac6b4cc9ecd0fc2
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-08-25 14:50:18 -0700

    Array Challenge

---
+++ ArrayChallenge/ArrayChallenge/ArrayChallenge.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    using namespace std;
+
+    string fruits[3] = { "Dragon-Fruit", "Star-Fruit", "Mango" };
+
+    cout << fruits[0] << "\n" << fruits[1] << "\n" << fruits[2] << "\n";
+}
