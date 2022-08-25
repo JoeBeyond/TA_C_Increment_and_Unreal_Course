f80c5d78600c320e79da90ef16fee9263aceb96a
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-08-25 14:09:20 -0700

    For Loop Challenge

---
+++ ForLoopChallenge/ForLoopChallenge/ForLoopChallenge.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    for (int dimensions = 0; dimensions <= 10; dimensions++)
+    {
+        cout << dimensions << " Dimensions!" << "\n";
+    }
+
+    cout << "Holy Mogus!";
+}
