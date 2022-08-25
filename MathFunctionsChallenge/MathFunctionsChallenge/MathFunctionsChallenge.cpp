f0eb3a0c8e8bda17ce77c15d98a89dc4f95205e4
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-08-25 01:29:33 -0700

    Math Functions Challenge

---
+++ MathFunctionsChallenge/MathFunctionsChallenge/MathFunctionsChallenge.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+
+int main()
+{
+    using namespace std;
+
+    cout << "MAX: " << max(22.5, 27.5) << '\n';
+
+    cout << "SQRT: " << sqrt(128) << '\n';
+
+    cout << "ROUND: " << round(8.4937) << '\n';
+
+    cout << "POWER: " << pow(101, 3) << '\n';
+}
