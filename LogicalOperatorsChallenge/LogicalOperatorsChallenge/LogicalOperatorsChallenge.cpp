2fe2374f9c469b4b85be13c70243db9af509f453
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-08-24 23:01:30 -0700

    Logical Operators Challenge

---
+++ LogicalOperatorsChallenge/LogicalOperatorsChallenge/LogicalOperatorsChallenge.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 10;
+
+    int num2 = 20;
+
+    int num3 = 30;
+
+    if (num1 != num2 || num3 == num3)
+    {
+        cout << "Have a coffee!";
+    }
+}
