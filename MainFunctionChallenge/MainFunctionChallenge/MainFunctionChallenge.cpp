7b8f59a01fd66034dba5dfbd4baa49fdc82848f6
JoeBeyond <97361279+JoeBeyond@users.noreply.github.com>
2022-08-26 02:07:49 -0700

    Main Function Challenge

---
+++ MainFunctionChallenge/MainFunctionChallenge/MainFunctionChallenge.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Sharky();
+void numberOfSharks();
+
+int main()
+{
+    numberOfSharks();
+
+    Sharky();
+}
+
+void Sharky()
+{
+    string str1 = "Sharks";
+
+    cout << str1 << "\n";
+}
+
+void numberOfSharks()
+{
+    int num1 = 33;
+
+    cout << num1 << "\n";
+}
